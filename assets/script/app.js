0627f68b3c88003d3b2048a70d74fd33f1bed3ce
Tobin Titus <tobin.titus@gmail.com>
2013-11-13 02:14:40 -0800

    add: home experience
    Add the home experience

---
+++ assets/script/app.js
@@ -0,0 +1,90 @@
+var exp = "";
+
+
+function write(data) {
+  exp += (data);
+}
+
+function writeLine(data) {
+  write(data + "<br/>");
+}
+
+function writeTechnologies(data) {
+  var output = "";
+
+  for(var s in data) {
+    output += "<li><a href=\"#" + data[s] + "\">" + data[s] + "</a></li>\r\n";
+  }
+
+  document.getElementById("technologies").innerHTML = output;
+}
+
+function writeTopics(data) {
+  var output = "";
+
+  for(var s in data) {
+    output += "<li><a href=\"#" + data[s] + "\">" + data[s] + "</a></li>\r\n";
+  }
+
+  document.getElementById("topics").innerHTML = output;
+}
+
+function bold(data) {
+  return "<b>" + data + "</b>";
+}
+  
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+};
+
+function loop(dataArray, filter) {
+  var count = 0;
+  var output = "";
+  var technologies = [];
+  var topics = [];
+
+  for(var d in dataArray) {
+    output +="<article  class=\"col-lg-3 col-md-3 col-sm-3 col-xs-12\" tabindex=\"" + (count+1) + "\"><header>";
+    output += "<h3><a href=\"" + dataArray[d].file + "\" tabindex='-1'>" + dataArray[d].name + "</a></h3>";
+    output += "<h6>" + dataArray[d].area + "</h6>"
+    output += "</header></article>";
+
+    technologies  = technologies.concat(dataArray[d].technologies.split(','));
+    topics        = topics.concat(dataArray[d].topics.split(","));
+
+    count++;
+  }
+
+
+  write(output);
+  writeTechnologies(arrayUnique(technologies).sort());
+  writeTopics(arrayUnique(topics).sort());
+}
+
+var loadExperiments = function() {
+  var content = document.querySelector('div[role="content"] div:first-child');
+  if(content) {
+    loop(experiments);
+  }
+  content.innerHTML = exp;
+}
+
+function load() {
+  loadExperiments();
+}
+
+function navigate() {
+  alert("hello");
+}
+
+window.addEventListener("load", load, false);
+window.addEventListener("navigate", navigate, false);
+
---
+++ assets/script/datalist.js
@@ -0,0 +1,84 @@
+var oldExperiments = [	
+{
+		"area"			: "canvas",
+		"name" 			: "Basic Animation",
+		"file" 			: "experiments/html5/canvas/animation/basic-animation.html",
+		"description" 	: "Basic movement of an item across HTML5 canvas."
+	},
+	{
+		"area"			: "canvas",
+		"name" 			: "Transform animation",
+		"file" 			: "experiments/html5/canvas/animation/transform.html",
+		"description" 	: "Animating and transforming (scale & rotate) an item across HTML5 canvas."
+	},
+	{
+		"area"			: "canvas",
+		"name" 			: "Compositing and Alpha Operations",
+		"file" 			: "experiments/html5/canvas/composite/composite.html",
+		"description" 	: "Compositing operations and globalAlpha in HTML5 canvas."
+	},
+	{
+		"area"			: "canvas",
+		"name" 			: "Parallax Game Movement",
+		"file" 			: "experiments/html5/canvas/parallax/game.html",
+		"description" 	: "Parallax movement in a game-like scenario on HTML5 canvas."
+	},
+	{
+		"area"			: "canvas",
+		"name" 			: "Basic Velocity",
+		"file" 			: "experiments/html5/canvas/physics/move.html",
+		"description" 	: "Basic velocity in HTML5 canvas."
+	},
+	{
+		"area"			: "canvas",
+		"name" 			: "Velocity with Reaction",
+		"file" 			: "experiments/html5/canvas/physics/vector-bounce.html",
+		"description" 	: "Velocity and reactive bounce in HTML5 canvas."
+	}
+];
+
+var	experiments = 
+[
+	{
+		"area"			: "offline",
+		"technologies"	: "HTML5,JavaScript",
+		"topics"		: "Data",
+		"name" 			: "Basic Web Storage",
+		"file" 			: "experiments/html5/webStorage/basic.html",
+		"description" 	: "Web localStorage and the onsubmit event."
+	},					
+	{
+		"area"			: "offline",
+		"technologies"	: "HTML5,JavaScript",
+		"topics"		: "Data",
+		"name" 			: "Advanced Web Storage",
+		"file" 			: "experiments/html5/webStorage/advanced.html",
+		"description" 	: "Web localStorage and sessionStorage using onsubmit event."
+	},
+	{
+		"area"			: "css3",
+		"technologies"	: "CSS",
+		"topics"		: "Style",
+		"name"			: "Breadcrumb",
+		"file"			: "experiments/css3/breadcrumb/breadcrumb.html",
+		"description"	: "Demonstrates how to rotate an item and use a gradient to get a menu separator effect."
+	},
+	{
+		"area"			: "css3",
+		"technologies"	: "CSS",
+		"topics"		: "Style",
+		"name"			: "Pointillism",
+		"file"			: "experiments/css3/pointillism/default.html",
+		"description"	: "Uses multiple gradients on a single element to render a bitmap."
+	},
+	{
+		"area"			: "performance",
+		"technologies"	: "JavaScript,CSS",
+		"topics"		: "Performance,Animation",
+		"name"			: "Animation Types",
+		"file"			: "experiments/performance/animationTypes/default.html",
+		"description"	: "Demonstrates the differences in dependent and independent animations."
+	}
+];
+
+	
